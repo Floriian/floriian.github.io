0e533c9067c5de841590547c28f1f610b5e48558
Floriian <lovasifloriian@outlook.hu>
2023-02-04 11:15:24 +0100

    basic stack

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Menu from './Menu';
 
 export default function Layout() {
   return (
-    <div className="w-screen h-screen bg-zinc-800 text-white">
-      <Outlet />
+    <div className="h-full w-full bg-zinc-800 text-white">
+      <Menu />
+      <div className="p-5">
+        <Outlet />
+      </div>
     </div>
   );
 }
