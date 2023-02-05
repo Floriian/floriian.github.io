d2368d0f200ca993be4c3c349704c40f82b54bf6
Floriian <lovasifloriian@outlook.hu>
2023-02-05 23:55:29 +0100

    roadmap animation (TODO)

--- src/components/RoadMap.tsx
+++ src/components/RoadMap.tsx
@@ -13,12 +13,16 @@ export function RoadMap() {
           return b.startDate - a.startDate;
         })
         .map((rmap) => (
-          <li
+          <motion.li
             ref={ref}
             className="mb-10 ml-4"
             key={rmap.description}
+            animate={inView ? { x: 0, opacity: 1 } : { x: 1, opacity: 0 }}
+            transition={{
+              duration: 0.3,
+            }}
           >
-            <div className="absolute -left-1.5 mt-1.5 h-6 w-6 text-xl">
+            <div className="absolute -left-2 mt-2 h-6 w-6 text-xl">
               {rmap.type === 'SCHOOL' ? <FcGraduationCap /> : <MdWork />}
             </div>
             <time className="text-lg text-zinc-500">
@@ -26,7 +30,7 @@ export function RoadMap() {
             </time>
             <h3 className="text-xl">{rmap.name}</h3>
             <p className="text-zinc-500">{rmap.description}</p>
-          </li>
+          </motion.li>
         ))}
     </ol>
   );
